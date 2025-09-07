5bbf982783a3b899f158e644347d800cdac0f02c
Avi-Kedare-75 <avikedare27@gmail.com>
2025-09-07 18:41:24 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,14 +2,15 @@ const mysql = require("mysql2");
 
 const db = mysql.createConnection({
   host: "localhost",
-  user: "root",                // your MySQL username
-  password: "Avikedareusv2",// your MySQL root password
+  user: "root",               
+  password: "Avikedareusv2",
   database: "authdb"
 });
 
 db.connect(err => {
   if (err) throw err;
-  console.log("✅ MySQL Connected");
+  console.log("MySQL Connected");
 });
 
 module.exports = db;
+
