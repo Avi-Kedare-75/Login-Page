468d96be212f7d18f5c37c03120ff09f4997f69d
Avi-Kedare-75 <avikedare27@gmail.com>
2025-09-07 18:41:52 +0530

    Update mailer.js

--- mailer.js
+++ mailer.js
@@ -1,12 +1,13 @@
 const nodemailer = require("nodemailer");
 
-// Using Gmail
+
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "avikedare27@gmail.com",       // replace with your email
-    pass: "iiufleonnkxisfed"           // App password if 2FA enabled
+    user: "avikedare27@gmail.com",       
+    pass: "iiufleonnkxisfed"           
   }
 });
 
 module.exports = transporter;
+
